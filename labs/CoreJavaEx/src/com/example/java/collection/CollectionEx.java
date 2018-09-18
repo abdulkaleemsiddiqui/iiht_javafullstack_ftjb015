9f96ec20c700379beb76ed88a6b3a03704faa698
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2018-09-18 10:07:42 +0530

    Java collection example updated

--- labs/CoreJavaEx/src/com/example/java/collection/CollectionEx.java
+++ labs/CoreJavaEx/src/com/example/java/collection/CollectionEx.java
@@ -8,7 +8,9 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -16,33 +18,37 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
-public class CollectionEx 
-{
-	public static void main(String[] args) 
-	{
+public class CollectionEx {
+
+	public static void main(String[] args) {
+
+		List countries = new ArrayList();
+		countries.add("India");
+		countries.add("China");
+		countries.add("USA");
+		countries.add("Brazil");
+		countries.add("India");
+		displayAll(countries);
+
 		List lnkLst = new LinkedList();
 		lnkLst.add("element1");
 		lnkLst.add("element2");
 		lnkLst.add("element3");
 		lnkLst.add("element4");
-		displayAll(lnkLst);
+		displayListEntries(lnkLst);
 
-		List aryLst = new ArrayList();
-		aryLst.add("x");
-		aryLst.add("y");
-		aryLst.add("z");
-		aryLst.add("w");
-		displayAll(aryLst);
+		// Hashing the element location for faster access		
+		Set hashSet = new HashSet(); 
 
-		Set hashSet = new HashSet();
 		hashSet.add("set1");
 		hashSet.add("set2");
 		hashSet.add("set3");
 		hashSet.add("set4");
 		displayAll(hashSet);
 
-		SortedSet treeSet = new TreeSet();
+		SortedSet treeSet = new TreeSet(); // Natural Sorting
 		treeSet.add("1");
 		treeSet.add("2");
 		treeSet.add("3");
@@ -61,6 +67,7 @@ public class CollectionEx
 		map1.put("key2", "K");
 		map1.put("key3", "L");
 		map1.put("key4", "M");
+		displayMapEntries(map1.entrySet());
 		displayAll(map1.keySet());
 		displayAll(map1.values());
 
@@ -69,6 +76,7 @@ public class CollectionEx
 		map2.put("key2", "KK");
 		map2.put("key3", "LL");
 		map2.put("key4", "MM");
+		displayMapEntries(map2.entrySet());
 		displayAll(map2.keySet());
 		displayAll(map2.values());
 
@@ -77,6 +85,7 @@ public class CollectionEx
 		map3.put("key2", "KKK");
 		map3.put("key3", "LLL");
 		map3.put("key4", "MMM");
+		displayMapEntries(map3.entrySet());
 		displayAll(map3.keySet());
 		displayAll(map3.values());
 	}
@@ -87,6 +96,29 @@ public class CollectionEx
 			String str = (String) itr.next();
 			System.out.print(str + " ");
 		}
+
+		System.out.println();
+	}
+
+	static void displayListEntries(List list) {
+		ListIterator itr = list.listIterator();
+
+		while (itr.hasNext()) {
+			String str = (String) itr.next();
+			System.out.print(str + " ");
+		}
+
+		System.out.println();
+	}
+
+	static void displayMapEntries(Set set) {
+		Iterator itr = set.iterator();
+
+		while (itr.hasNext()) {
+			Map.Entry entry = (Map.Entry) itr.next();
+			System.out.println(entry.getKey() + " " + entry.getValue() + " ");
+		}
+
 		System.out.println();
 	}
 }
